6032770db82391c90f7f5cacda4ac877bbb9ceae
Mariusz <mpostol@cas.eu>
2016-12-23 20:20:18 +0000

    Port DNN mail list import - added functionality to manage the DNN database - added functionality to recognize the temporal emails - unit tested. Issue #: 4890

---
+++ DNNDataBase.DataBaseManagement/BurnerEmailsProvidersServices.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+
+namespace CAS.DNNDataBase.DataBaseManagement
+{
+  public static class BurnerEmailsProvidersServices
+  {
+    /// <summary>
+    /// Determines whether the the specified domain is registered.
+    /// </summary>
+    /// <param name="domain">The domain to check if it is registered.</param>
+    /// <returns><c>true</c> if the domain is registered; otherwise, <c>false</c>.</returns>
+    public static bool IsItRegisteredProvider(string domain)
+    {
+      return m_RegisteredProviders.Value.Contains(domain);
+    }
+    public static bool IsItRegisteredProvider(MailAddress address)
+    {
+      return m_RegisteredProviders.Value.Contains(address.Host);
+    }
+    private static Lazy<List<string>> m_RegisteredProviders = new Lazy<List<String>>(() => GetProviders());
+    private static List<string> GetProviders()
+    {
+      String[] _list = Properties.Resources.BurnerEmailsProviders.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+      List<string> _listProviders = new List<string>(_list);
+      _listProviders.Sort();
+      return _listProviders;
+    }
+
+  }
+}
---
+++ DNNDataBase.DataBaseManagement/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿//_______________________________________________________________
+//  Title   :  Assembly info for: CAS.DNNDataBase.DataBaseManagement
+//  System  : Microsoft VisualStudio 2015 / C#
+//  $LastChangedDate:  $
+//  $Rev:  $
+//  $LastChangedBy: $
+//  $URL:  $
+//  $Id:  $
+//
+//  Copyright (c) 2000-2016 CAS LODZ POLAND
+//  +48 (42) 686 25 47
+//  techsupp@cas.eu
+//  www.cas.eu
+//_______________________________________________________________
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CAS.DNNDataBase.DataBaseManagement")]
+[assembly: AssemblyDescription("CAS DNN Database Management Library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CAS")]
+[assembly: AssemblyProduct("CAS Framework Core")]
+[assembly: AssemblyCopyright("Copyright (c) 2000-2016 CAS LODZ POLAND")]
+[assembly: AssemblyTrademark("CAS Framework")]
+[assembly: AssemblyCulture("")]
+[assembly: Guid("9eb44a90-a66b-4892-940b-fbd904edba56")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("2.00.00.*")]
+[assembly: AssemblyFileVersion("2.00.00")]
+
+
