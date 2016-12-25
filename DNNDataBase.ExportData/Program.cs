d3e80c19a10ef14f9120d3285894e4986bd895ff
Mariusz <mpostol@cas.eu>
2016-12-25 12:42:43 +0000

    Port DNN mail list import - added tool to export list of emails. - unit tested. Issue #: 4890

---
+++ DNNDataBase.ExportData/Program.cs
@@ -0,0 +1,64 @@
+﻿//_______________________________________________________________
+//  Title   : Entry point for the DNNDataBase.ExportData
+//  System  : Microsoft VisualStudio 2015 / C#
+//  $LastChangedDate:  $
+//  $Rev: $
+//  $LastChangedBy: $
+//  $URL: $
+//  $Id:  $
+//
+//  Copyright (C) 2016, CAS LODZ POLAND.
+//  TEL: +48 (42) 686 25 47
+//  mailto://techsupp@cas.eu
+//  http://www.cas.eu
+//_______________________________________________________________
+
+using CAS.DNNDataBase.DataBaseManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CAS.DNNDataBase.ExportData
+{
+  public class Program
+  {
+
+    public static void Main(string[] args)
+    {
+      Task<IEnumerable<DNNDataClassesDataContext.UserEmail>> _listOfEmailsTask = GetEmailAsync();
+      try
+      {
+        _listOfEmailsTask.Wait();
+        IEnumerable<DNNDataClassesDataContext.UserEmail> _listOfEmails = _listOfEmailsTask.Result;
+        string _finalList = String.Join<string>(";", _listOfEmails.Select<DNNDataClassesDataContext.UserEmail, string>(_email => _email.Email).ToArray<string>());
+        Console.WriteLine();
+        Console.WriteLine("Final list of emails:");
+        Console.WriteLine(_finalList);
+        Console.WriteLine();
+      }
+      catch (AggregateException _ex)
+      {
+        foreach (Exception _item in _ex.InnerExceptions)
+          Console.WriteLine(_listOfEmailsTask.Exception.ToString());
+      }
+      catch (Exception _ge)
+      {
+        Console.WriteLine(_ge.ToString());
+      }
+      Console.Write("Press enter to close the window:");
+      Console.ReadLine();
+    }
+    private static async Task<IEnumerable<DNNDataClassesDataContext.UserEmail>> GetEmailAsync()
+    {
+      Task<IEnumerable<DNNDataClassesDataContext.UserEmail>> _emails = DNNDataClassesDataContext.GetEmailAsync(DateTime.Now - TimeSpan.FromDays(10));
+      while (!_emails.IsCompleted)
+      {
+        Thread.Sleep(1000);
+        Console.Write(".");
+      }
+      return await _emails;
+    }
+  }
+}
---
+++ DNNDataBase.ExportData/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿//_______________________________________________________________
+//  Title   :  Assembly info for: CAS.DNNDataBase.ExportData
+//  System  : Microsoft VisualStudio 2015 / C#
+//  $LastChangedDate:  $
+//  $Rev:  $
+//  $LastChangedBy: $
+//  $URL:  $
+//  $Id:  $
+//
+//  Copyright (c) 2000-2016 CAS LODZ POLAND
+//  +48 (42) 686 25 47
+//  techsupp@cas.eu
+//  www.cas.eu
+//_______________________________________________________________
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CAS.DNNDataBase.ExportData")]
+[assembly: AssemblyDescription("CAS DNN Database Data Exporting Tool")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CAS")]
+[assembly: AssemblyProduct("CAS Framework Core")]
+[assembly: AssemblyCopyright("Copyright (c) 2000-2016 CAS LODZ POLAND")]
+[assembly: AssemblyTrademark("CAS Framework")]
+[assembly: AssemblyCulture("")]
+[assembly: Guid("e1c573a3-d4be-49a6-9564-6512a54db9a6")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("2.00.00.*")]
+[assembly: AssemblyFileVersion("2.00.00")]
+
+
