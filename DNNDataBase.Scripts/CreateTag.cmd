5aa2bcae7991c3099885d99d5823f8eff1a534da
Mariusz <mpostol@cas.eu>
2016-12-28 13:46:24 +0000

    Port DNN mail list import - added project containing scripts - added script creating tag. Issue #: 4890

--- DNNDataBase.Scripts/CreateTag.cmd
+++ DNNDataBase.Scripts/CreateTag.cmd
@@ -6,8 +6,8 @@ rem//  $URL:  $
 rem//  $Id:  $
 
 
-svn mkdir svn://svnserver.hq.cas.com.pl/VS/tags/CommServer.UA.Stack.Core.rel_1_01_3265  -m "created new svn://svnserver.hq.cas.com.pl/VS/tags/CommServer.UA.Stack.Core.rel_1_01_3265"
-svn copy svn://svnserver.hq.cas.com.pl/VS/trunk/CommServer.UA.Stack.Core/ svn://svnserver.hq.cas.com.pl/VS/tags/CommServer.UA.Stack.Core.rel_1_01_3265 -m "created copy svn://svnserver.hq.cas.com.pl/VS/tags/CommServer.UA.Stack.Core.rel_1_01_3265"
+svn mkdir svn://svnserver.hq.cas.com.pl/VS/tags/CAS.DNNDatabase.rel_2_00_00  -m "created new svn://svnserver.hq.cas.com.pl/VS/tags/CAS.DNNDatabase.rel_2_00_00"
+svn copy svn://svnserver.hq.cas.com.pl/VS/trunk/CAS.DNNDatabase/ svn://svnserver.hq.cas.com.pl/VS/tags/CAS.DNNDatabase.rel_2_00_00 -m "created copy svn://svnserver.hq.cas.com.pl/VS/tags/CAS.DNNDatabase.rel_2_00_00"
 
 pause ....
 
