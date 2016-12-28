f53d08f1bb0cdb20063f24c9ad0a00750c431c4c
Mariusz <mpostol@cas.eu>
2016-12-28 13:28:48 +0000

    Port DNN mail list import - email export is ready to be used. Issue #: 4890

---
+++ DNNDataBase.Scripts/CreateTag.cmd
@@ -0,0 +1,13 @@
+
+rem//  $LastChangedDate:  $
+rem//  $Rev:  $
+rem//  $LastChangedBy:  $
+rem//  $URL:  $
+rem//  $Id:  $
+
+
+svn mkdir svn://svnserver.hq.cas.com.pl/VS/tags/CommServer.UA.Stack.Core.rel_1_01_3265  -m "created new svn://svnserver.hq.cas.com.pl/VS/tags/CommServer.UA.Stack.Core.rel_1_01_3265"
+svn copy svn://svnserver.hq.cas.com.pl/VS/trunk/CommServer.UA.Stack.Core/ svn://svnserver.hq.cas.com.pl/VS/tags/CommServer.UA.Stack.Core.rel_1_01_3265 -m "created copy svn://svnserver.hq.cas.com.pl/VS/tags/CommServer.UA.Stack.Core.rel_1_01_3265"
+
+pause ....
+
