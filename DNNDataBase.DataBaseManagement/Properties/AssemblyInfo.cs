2ab737040d873b2799f9c998a1f87739ff477347
mpostol <mpostol@cas.eu>
2020-01-02 23:37:39 +0100

    Fixed text transformation errors UT :-1: - depend on the DB direct connection

--- DNNDataBase.DataBaseManagement/Properties/AssemblyInfo.cs
+++ DNNDataBase.DataBaseManagement/Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@
 //  $URL:  $
 //  $Id:  $
 //
-//  Copyright (c) 2000-2016 CAS LODZ POLAND
-//  +48 (42) 686 25 47
-//  techsupp@cas.eu
+//  Copyright (C) 2019, Mariusz Postol LODZ POLAND.
+//  +48 (608) 619 899
+//  mpostol@cas.eu
 //  www.cas.eu
 //_______________________________________________________________
 
@@ -20,9 +20,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("CAS.DNNDataBase.DataBaseManagement")]
 [assembly: AssemblyDescription("CAS DNN Database Management Library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("CAS")]
+[assembly: AssemblyCompany("Mariusz Postol")]
 [assembly: AssemblyProduct("CAS Framework Core")]
-[assembly: AssemblyCopyright("Copyright (c) 2000-2016 CAS LODZ POLAND")]
+[assembly: AssemblyCopyright("Copyright (C) 2019, Mariusz Postol LODZ POLAND.")]
 [assembly: AssemblyTrademark("CAS Framework")]
 [assembly: AssemblyCulture("")]
 [assembly: Guid("9eb44a90-a66b-4892-940b-fbd904edba56")]
--- DNNDataBase.UsersManagement/Properties/AssemblyInfo.cs
+++ DNNDataBase.UsersManagement/Properties/AssemblyInfo.cs
@@ -1,15 +1,15 @@
 ﻿//_______________________________________________________________
 //  Title   :  Assembly info for: CAS.DNNDataBase.UsersManagement
 //  System  : Microsoft VisualStudio 2015 / C#
-//  $LastChangedDate$
-//  $Rev$
-//  $LastChangedBy$
-//  $URL$
-//  $Id$
+//  $LastChangedDate:  $
+//  $Rev:  $
+//  $LastChangedBy: $
+//  $URL:  $
+//  $Id:  $
 //
-//  Copyright (c) 2000-2016 CAS LODZ POLAND
-//  +48 (42) 686 25 47
-//  techsupp@cas.eu
+//  Copyright (C) 2019, Mariusz Postol LODZ POLAND.
+//  +48 (608) 619 899
+//  mpostol@cas.eu
 //  www.cas.eu
 //_______________________________________________________________
 
@@ -20,9 +20,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("CAS.DNNDataBase.UsersManagement")]
 [assembly: AssemblyDescription("CAS DNN Users Database Management Application")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("CAS")]
+[assembly: AssemblyCompany("Mariusz Postol")]
 [assembly: AssemblyProduct("CAS Framework Core")]
-[assembly: AssemblyCopyright("Copyright (c) 2000-2016 CAS LODZ POLAND")]
+[assembly: AssemblyCopyright("Copyright (C) 2019, Mariusz Postol LODZ POLAND.")]
 [assembly: AssemblyTrademark("CAS Framework")]
 [assembly: AssemblyCulture("")]
 [assembly: Guid("c54a8671-c7f9-4b2d-9344-a3f4df247239")]
